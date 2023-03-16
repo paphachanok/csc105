e5d539b5ec3cdad6ca8f36aec2023d3579392f55
Paphachanok Poti <phapachanok30185@gmail.com>
2023-03-16 11:45:49 +0700

    16 march 2023

---
+++ todo_app/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import "./Profile.css";
+
+function Profile() {
+  return (
+	<div className="layout">
+		<img src="src/assets/Tong.jpg" alt="profile" />
+		<h1 className="name" >Paphachanok Poti</h1>
+		<hr></hr>
+	</div>
+  )
+}
+
+export default Profile
