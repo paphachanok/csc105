4651eb69f426e11b01b3c6353adb373b59f21703
Paphachanok Poti <phapachanok30185@gmail.com>
2023-03-29 00:24:11 +0700

    todo3_responsive

---
+++ todo3_mui_responsive/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import "./App.css";
+import TodoPage from "./pages/TodoPage";
+
+function App() {
+
+  return (
+	<div className='App'>
+		<TodoPage />
+	</div>
+  )
+}
+
+export default App
